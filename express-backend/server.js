7e71a322cf858ed78c2c87b1a146c7e8d678c660
ramin <Ramin.Nourbakhsh@fornida.com>
2020-01-09 09:57:43 -0600

    backend setup!

--- express-backend/server.js
+++ express-backend/server.js
@@ -1,7 +1,77 @@
 const express = require('express')
+const colors = require('colors')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/images', (req, res) => res.send(db))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`.blue))
+
+const db = {
+    'headshots': [
+        {
+            'name': "ramin",
+            'desc': "blabla",
+            'url': "lol.com",
+        },
+        {
+            'name': "",
+            'desc': "",
+            'url': "",
+        },
+        {
+            'name': "",
+            'desc': "",
+            'url': "",
+        },
+        {
+            'name': "",
+            'desc': "",
+            'url': "",
+        },
+    ],
+    'dataing_profile': [
+        {
+            'name': "",
+            'desc': "",
+            'url': "",
+        },
+        {
+            'name': "",
+            'desc': "",
+            'url': "",
+        },
+        {
+            'name': "",
+            'desc': "",
+            'url': "",
+        },
+        {
+            'name': "",
+            'desc': "",
+            'url': "",
+        },
+    ],
+    'landscaps': [
+        {
+            'name': "",
+            'desc': "",
+            'url': "",
+        },
+        {
+            'name': "",
+            'desc': "",
+            'url': "",
+        },
+        {
+            'name': "",
+            'desc': "",
+            'url': "",
+        },
+        {
+            'name': "",
+            'desc': "",
+            'url': "",
+        },
+    ]
+}
