9a392388fab68fa409b186eca78b48adca54f848
odfornida <oded.falik@fornida.com>
2020-01-09 10:17:12 -0600

    second commit

--- ng-frontend/src/app/app.module.ts
+++ ng-frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    GalleryComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
