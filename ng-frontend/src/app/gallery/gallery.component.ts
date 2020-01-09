a242f092214e5a308b7f36acbb9b4b373edfbd54
thomas <thomas.babic@fornida.com>
2020-01-09 12:24:00 -0600

    Completed photo service and getImages in gallery component

--- ng-frontend/src/app/gallery/gallery.component.ts
+++ ng-frontend/src/app/gallery/gallery.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PhotosService } from '../photos.service';
 
 @Component({
-  selector: 'app-gallery',
+  selector: 'gallery',
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.scss']
 })
@@ -12,19 +12,17 @@ export class GalleryComponent implements OnInit {
   photo_desc : string;
   photo_url : string;
 
-  constructor(private photoService: PhotosService) { }
+  constructor(private photoService: PhotosService) {   }
 
   ngOnInit() {
-
+    this.getImages();
   }
-
+  getImages()
+  {
+    return this.photoService;
+  }
   setPhotoType(photo_category : string)
   {
       this.photo_type = photo_category;
-  }
-
-  get_images()
-  {
-
   }
 }
--- ng-frontend/src/app/photos.service.ts
+++ ng-frontend/src/app/photos.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable, getDebugNode } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhotosService {
+  photo_api = 'localhost:3000';
+  photos : [];
+  constructor(private http: HttpClient) {}
+  get_images()
+    {
+      this.http.get<any>(this.photo_api + 'images').toPromise().then(data => 
+      {
+        this.photos = data;
+        console.log(this.photos);
+      });
+      return this.photos;
+    }
 
-  constructor() { }
 }
