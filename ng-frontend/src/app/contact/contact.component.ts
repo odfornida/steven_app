493f3463c94ab9ea05ba47ae9a0a7f4565527996
ramin <Ramin.Nourbakhsh@fornida.com>
2020-01-10 09:58:09 -0600

    forms improved

--- ng-frontend/src/app/contact/contact.component.ts
+++ ng-frontend/src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PhotosService } from '../photos.service';
 
 @Component({
   selector: 'contact',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  constructor(public photoService: PhotosService) { }
 
   ngOnInit() {
+    console.log(this.photoService.getCategories())
   }
 
 }
